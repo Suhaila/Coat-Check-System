8876516744c8b88277e2cda2676e90e0255b29b3
Suhaila B <skbaheye@gmail.com>
2012-02-22 00:20:04 -0500

    Updated README and comments in Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,21 @@
 # This is the main script for our Coat Check System
+# Read the README for how to run the script :)
 
-import csv # imports the 'csv' module so that you can use it
+# importing modules to be used in the script
+import csv 
+import serial
 
-csvFile = 'Sample_data.csv' # defines the csv file with the data
+# define some variables
+csvFile = 'Sample_data.csv'
 
+# Opens the csvFile, reads the data, and prints it on the screen
 with open(csvFile, "rb") as file:
 	reader = csv.reader(file)
-	for row in reader:
+	for row in reader:		# for loop to read each row in the file
 		print row
 
- 
-with open(csvFile, 'ab') as f:
+# Append a new row to csvFile (hardcoded for now until we get input from a barcode scanner) 
+with open(csvFile, 'ab') as f:		# open file for appending - 'ab'
     writer = csv.writer(f)
     writer.writerow([004, 'Alex', 'Mann', 456789012])
 
